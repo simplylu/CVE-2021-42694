e66b36cb0741381a63a4043bb77345a043d34e13
js-on <jakobs@js-on.de>
2021-11-02 20:31:43 +0000

    initial commit

---
+++ examples/hello_world.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+void sayHello() {
+    std::cout << "Hello, World!\n";
+}
+
+void say$H$ello() {
+    std::cout << "Goodbye, World!\n";
+}
+
+int main() {
+    sayHello();
+    say$H$ello();
+    return 0;
+}
---
+++ examples/hello_world.py
@@ -0,0 +1,8 @@
+def sayHello():
+    print("Hello, World!\n")
+
+def say$H$ello():
+    print("Goodbye, World!\n")
+
+sayHello()
+say$H$ello()
